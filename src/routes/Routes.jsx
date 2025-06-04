5142a9f2e85c9fd59b7ee44e14f689306d93beb1
Jafet-a11 <145033871+Jafet-a11@users.noreply.github.com>
2025-06-04 12:01:41 -0600

    Se agregan la vista de division

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import { ProgramaEducativo } from "../pages/ProgramaEducativo";
-
+import  Division  from "../pages/Division";
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<ProgramaEducativo />} />
+            <Route path="/divisiones" element={<Division />} />
         </Routes>
     );
 };
